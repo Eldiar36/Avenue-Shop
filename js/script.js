aee9ad933b8a45e1e05ec7e872f3f6d0152b2679
unknown <eldiar36>
2020-02-19 10:12:44 +0600

    initial2

--- js/script.js
+++ js/script.js
@@ -157,14 +157,22 @@ window.onscroll = function showHeader() {
     }
 };
 //
-$('.accordion').click(function () {
-    if ($(this).next().is(":visible")) {
-        $(this).next().hide('slow');
-    } else {
-        $('.panel:visible').hide('slow');
-        $(this).next().show('slow');
-    }
-})
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+
+        this.classList.toggle("active");
+
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
 //
 //wear-img-gallery
 let bigImg = document.getElementById('item-full');
